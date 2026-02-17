27177a3f51e93d004aba6ea33da40db8e1be892d
chand kaurati <132880291+chandkaurati@users.noreply.github.com>
2026-02-17 20:28:21 +0530

    Clean slate: Remove old code for refactor
    Deleted messy structure to rebuild with:
    - CSS Modules (scoped styles)
    - Reusable components
    - Proper React Router
    - Clean architecture

--- dailyui-react/src/App.tsx
+++ dailyui-react/src/App.tsx
@@ -1,34 +1,11 @@
-import { useState } from "react";
-import Navbar from "./components/Layout/Navbar";
-import Sidebar from "./components/Layout/Sidebar";
-import MainContent from "./components/Layout/MainContent";
-import Footer from "./components/Layout/Footer";
-import "./App.css";
-import { Outlet } from "react-router-dom";
+import React from 'react'
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [selectedComponent, setSelectedComponent] = useState("introduction");
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
   return (
-    <div className="app">
-      {/* Fixed Navbar at top */}
-      <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-      <main className="app-body">
-        <Sidebar
-          isOpen={isSidebarOpen}
-          selectedComponent={selectedComponent}
-          setSelectedComponent={setSelectedComponent}
-        />
-        <MainContent>{<Outlet />}</MainContent>
-      </main>
-      <Footer />
+    <div>
+      app
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
